760cf3fe1d22de506be55a0565a4bf01655f3baf
Karolis Stasaitis <stkarolis@gmail.com>
2018-05-30 08:57:01 +0200

    Mac OS Support

--- src/unix/system.c
+++ src/unix/system.c
@@ -46,6 +46,10 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include <SDL_main.h>
 #endif
 
+#ifdef __APPLE__
+#include <mach-o/dyld.h>
+#endif
+
 cvar_t  *sys_basedir;
 cvar_t  *sys_libdir;
 cvar_t  *sys_homedir;
@@ -285,6 +289,22 @@ void Sys_Init(void)
     signal(SIGTTOU, SIG_IGN);
     signal(SIGPIPE, SIG_IGN);
     signal(SIGUSR1, hup_handler);
+
+#ifdef __APPLE__
+   // set Q2Pro.app/Contents/Resources as basedir
+   char path[MAX_OSPATH], *c;
+   unsigned int i = sizeof(path);
+
+   if (_NSGetExecutablePath(path, &i) > -1) {
+       if ((c = strstr(path, "Q2Pro.app"))) {
+           strcpy(c, "Q2Pro.app/Contents/Resources");
+           Cvar_FullSet("basedir", path, CVAR_NOSET, FROM_CODE);
+
+           strcpy(c, "Q2Pro.app/Contents/MacOS");
+           Cvar_FullSet("libdir", path, CVAR_NOSET, FROM_CODE);
+       }
+   }
+#endif 
 
     // basedir <path>
     // allows the game to run from outside the data tree
