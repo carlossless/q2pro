b42441f6fcc2fac487344c7d5f59effaee7b41ae
Karolis Stasaitis <stkarolis@gmail.com>
2018-06-02 13:23:22 +0200

    64

--- scripts/win-package.sh
+++ scripts/win-package.sh
@@ -14,9 +14,9 @@ mkdir -p ${ROOT_DIR}
 mkdir -p ${BUILD_DIR}
 mkdir -p ${BASEQ2_DIR}
 
-cp q2pro ${BUILD_DIR}
-cp q2proded ${BUILD_DIR}
-cp gamex86_64.so ${BASEQ2_DIR}
+cp q2pro.exe ${BUILD_DIR}
+cp q2proded.exe ${BUILD_DIR}
+cp gamex86_64.dll ${BASEQ2_DIR}
 
 cp -r scripts/baseq2/* ${BASEQ2_DIR}
 cp -r src/client/ui/q2pro.menu ${BASEQ2_DIR}
