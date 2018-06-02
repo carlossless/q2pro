c4d25229ab2d41084cc3a8778065fdd6c423abb2
Karolis Stasaitis <stkarolis@gmail.com>
2018-06-02 14:26:43 +0200

    package up q2demo assets

--- scripts/win-package.sh
+++ scripts/win-package.sh
@@ -18,8 +18,8 @@ cp q2pro.exe ${BUILD_DIR}
 cp q2proded.exe ${BUILD_DIR}
 cp gamex86_64.dll ${BASEQ2_DIR}
 
-cp -r scripts/baseq2/* ${BASEQ2_DIR}
-cp -r src/client/ui/q2pro.menu ${BASEQ2_DIR}
+cp src/client/ui/q2pro.menu ${BASEQ2_DIR}
+tar zxf scripts/q2demo-baseq2.tar.gz -C ${BASEQ2_DIR}
 
 pushd ${BUILD_DIR}
 zip -r ../${APP_NAME}-win.zip .
