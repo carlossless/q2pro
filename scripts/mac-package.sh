941b008644fd8c43bc8016bc382aa8da890eb1cf
Karolis Stasaitis <stkarolis@gmail.com>
2018-06-03 18:28:57 +0200

    fixed resource packaging

--- scripts/mac-package.sh
+++ scripts/mac-package.sh
@@ -10,7 +10,8 @@ APP_DIR=${BUILD_DIR}/${APP_NAME}.app
 CONTENT_DIR=${APP_DIR}/Contents
 BIN_DIR=${CONTENT_DIR}/MacOS
 RESOURCE_DIR=${CONTENT_DIR}/Resources
-BASEQ2_DIR=${RESOURCE_DIR}/baseq2
+BASEQ2_BIN_DIR=${BIN_DIR}/baseq2
+BASEQ2_RESOURCE_DIR=${RESOURCE_DIR}/baseq2
 LIB_DIR=${CONTENT_DIR}/Library
 VOLUME_NAME=${APP_NAME}
 
@@ -23,16 +24,17 @@ mkdir -p ${CONTENT_DIR}
 mkdir -p ${BIN_DIR}
 mkdir -p ${RESOURCE_DIR}
 mkdir -p ${LIB_DIR}
-mkdir -p ${BASEQ2_DIR}
+mkdir -p ${BASEQ2_BIN_DIR}
+mkdir -p ${BASEQ2_RESOURCE_DIR}
 
 cp scripts/apple/Info.plist ${CONTENT_DIR}
 cp q2pro ${BIN_DIR}
 cp q2proded ${BIN_DIR}
-cp gamex86_64.so ${BIN_DIR}/baseq2
+cp gamex86_64.so ${BASEQ2_BIN_DIR}
 
 cp scripts/apple/q2pro.icns ${RESOURCE_DIR}
-cp src/client/ui/q2pro.menu ${BASEQ2_DIR}
-tar zxf scripts/q2demo-baseq2.tar.gz -C ${BASEQ2_DIR}
+cp src/client/ui/q2pro.menu ${BASEQ2_RESOURCE_DIR}
+tar zxf scripts/q2demo-baseq2.tar.gz -C ${BASEQ2_RESOURCE_DIR}
 
 dylibbundler -b \
     -x "${BIN_DIR}/q2pro" \
